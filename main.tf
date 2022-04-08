8e024e589218a0fb6a2709c33fba50c76c12280f
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-04-08 15:13:43 +0800

    Updated.

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 # Updated: Tue Apr  5 16:28:09 +08 2022
 
+# Intrinsic variables.
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
+}
+
 # Credentials variables.
 variable "aws_access_key_id" {
   type      = string
@@ -14,6 +22,38 @@ variable "aws_assumed_role_arn" {
   sensitive = true
 }
 
+# Tags variables.
+variable "tag_Name" {
+  type        = string
+  default     = "Terraform Cross-Account Test"
+  description = "Name tag."
+}
+variable "tag_sitecode" {
+  type        = string
+  default     = "123"
+  description = "Site code tag."
+}
+variable "tag_department" {
+  type        = string
+  default     = "CuriousCats"
+  description = "Department tag."
+}
+variable "tag_team" {
+  type        = string
+  default     = "myteam@mycompany.com"
+  description = "Team tag."
+}
+variable "tag_tier" {
+  type        = string
+  default     = "dev"
+  description = "Tier tag."
+}
+variable "tag_costcenter" {
+  type        = string
+  default     = "9999"
+  description = "Cost center tag."
+}
+
 # Main account provider data.
 provider "aws" {
   region     = "us-east-1"
@@ -21,12 +61,12 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
   default_tags {
     tags = {
-      Name       = "Terraform Cross-Account Test"
-      sitecode   = "123"
-      department = "CuriousCats"
-      team       = "myteam@mycompany.com"
-      tier       = "dev"
-      costcenter = "1234"
+      Name       = var.tag_Name
+      sitecode   = var.tag_sitecode
+      department = var.tag_department
+      team       = var.tag_team
+      tier       = var.tag_tier
+      costcenter = var.tag_costcenter
     }
   }
 }
@@ -37,6 +77,16 @@ provider "aws" {
   region     = "us-east-1"
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
+  default_tags {
+    tags = {
+      Name       = var.tag_Name
+      sitecode   = var.tag_sitecode
+      department = var.tag_department
+      team       = var.tag_team
+      tier       = var.tag_tier
+      costcenter = var.tag_costcenter
+    }
+  }
   assume_role {
     role_arn     = var.aws_assumed_role_arn
     session_name = "terraform-cross-account-test"
@@ -44,7 +94,9 @@ provider "aws" {
 }
 
 # Main account deployment.
-resource "aws_s3_bucket" "main" {}
+resource "aws_s3_bucket" "main" {
+  bucket = "mybucket-${local.account_id}-${local.region}"
+}
 resource "aws_s3_bucket_acl" "main" {
   bucket = aws_s3_bucket.main.id
   acl    = "private"
@@ -57,6 +109,7 @@ output "main_bucket_name" {
 # Sub-account deployment.
 resource "aws_s3_bucket" "sub" {
   provider = aws.cross-account
+  bucket   = "mybucket-${local.account_id}-${local.region}"
 }
 resource "aws_s3_bucket_acl" "sub" {
   provider = aws.cross-account
