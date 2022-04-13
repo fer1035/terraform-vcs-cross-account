8df6a275deda695c8b5814b5bcf101c9e7daece2
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-04-13 13:20:21 +0800

    Testing SCP restrictions.

--- sub_account.tf
+++ sub_account.tf
@@ -6,3 +6,21 @@ resource "aws_security_group" "terraform_sg" {
   description = "Test SG creation across accounts using Terraform."
   vpc_id      = "vpc-037792a989afc3b34"
 }
+
+resource "aws_iam_role" "container_worker_role" {
+  name = "ContainerWorker"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = "ContainerWorker"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
